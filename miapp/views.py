7522adeb95cb56d65c4d38bc1c6badf672f5cf3e
03yeison <rozorangelyeison@gmail.com>
2025-11-26 07:29:39 -0500

    first commit

---
+++ miapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def contacto(request):
+    return render(request, "1-index.html")
+
+def turismo(request):
+    return render(request, "2-index.html")
+from django.shortcuts import render
